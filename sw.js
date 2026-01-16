db868c29ecd76f7c6668a4a4b9e49be991c2242d
Robie <robie1373@gmail.com>
2026-01-15 21:10:07 -0500

    1.16.1

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump CACHE_VERSION with each deploy to bust old caches.
-const CACHE_VERSION = '1.16.0';
+const CACHE_VERSION = '1.16.1';
 const CACHE_NAME = `random-list-manager-v${CACHE_VERSION}`;
 const urlsToCache = [
   './',
