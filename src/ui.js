d11c303a1f67f9e663e07db17cf281714ece6976
Robie <robie1373@gmail.com>
2026-01-15 22:51:39 -0500

    fix: ensure prompt is hidden after adding items to inventory

--- src/ui.js
+++ src/ui.js
@@ -926,16 +926,22 @@ export const UI = {
         promptMessage.innerText = message;
         promptPrimary.innerText = inventoryTabs[0].name;
         promptPrimary.onclick = () => {
-            this.addToInventory(inventoryTabs[0].id, resultText);
-            this.hidePrompt();
+            try {
+                this.addToInventory(inventoryTabs[0].id, resultText);
+            } finally {
+                this.hidePrompt();
+            }
         };
 
         if (inventoryTabs.length > 1) {
             promptSecondary.innerText = inventoryTabs[1].name;
             promptSecondary.style.display = 'block';
             promptSecondary.onclick = () => {
-                this.addToInventory(inventoryTabs[1].id, resultText);
-                this.hidePrompt();
+                try {
+                    this.addToInventory(inventoryTabs[1].id, resultText);
+                } finally {
+                    this.hidePrompt();
+                }
             };
         }
 
@@ -943,8 +949,11 @@ export const UI = {
         if (inventoryTabs.length > 2) {
             promptCancel.innerText = inventoryTabs[2].name;
             promptCancel.onclick = () => {
-                this.addToInventory(inventoryTabs[2].id, resultText);
-                this.hidePrompt();
+                try {
+                    this.addToInventory(inventoryTabs[2].id, resultText);
+                } finally {
+                    this.hidePrompt();
+                }
             };
         } else {
             promptCancel.innerText = 'Cancel';
